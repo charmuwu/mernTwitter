12a2ad3e2bd0abb26c6c235ede60078d082546f0
Andrew Kim <andrewikim95@gmail.com>
2021-03-29 03:08:43 -0400

    add front end tweets

--- frontend/src/reducers/root_reducer.js
+++ frontend/src/reducers/root_reducer.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import session from './session_reducer';
+import TweetsReducer from './tweets_reducer';
 
 const RootReducer = combineReducers({
-  session
+  session,
+  tweets: TweetsReducer
 });
 
 export default RootReducer;
